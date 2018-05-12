0c85f475c1bc472791b7b4319dbd536028d2027a
sebastiangherghesanu <sebastian.gherghesanu@tribalworldwide.co.uk>
2018-05-12 16:15:41 +0300

    expressions

--- src/step3_expressions.scala
+++ src/step3_expressions.scala
@@ -2,7 +2,7 @@
   * “The smallest useful fragment of code in many programming languages
   * is either a statement or an expression. They’re different in a simple way.”
    statement: do not produce results, just changing state
-   expressions: produce a result
+   expressions: produce a result (general definition)
 
    everything is Scala is an expression
   */
@@ -19,11 +19,21 @@ object step3_expressions extends App{
     i + j
   }
 
-  println(z)
+  //println doesn't produce a value : Unit
+  var classOf = println(z).getClass
+  println(classOf)
+  val nada = {}
+  println(nada)
 
+  val out = println(z)
+  out
 
 }
 /*
 5
 27
+void
+()
+27
+
  */
