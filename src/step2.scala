f3251f56a5d10db767b883b4cb8356aec1c02a81
sebastiangherghesanu <sebastian.gherghesanu@tribalworldwide.co.uk>
2018-05-12 15:46:36 +0300

    var

---
+++ src/step2.scala
@@ -0,0 +1,12 @@
+//**//“Like a value, a variable holds a particular type of information.
+// But with a variable, you are allowed to change the data that is stored”
+
+Excerpt From: Bruce Eckel & Dianne Marsh. “Atomic Scala V1.1.” iBooks.
+
+object step2 extends App{
+  var koko = "My name is Koko"
+  println(koko)
+  koko = "My name is Koko Bow"
+  println(koko)
+
+}
